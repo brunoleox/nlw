721e9a6f075ea87c806cde85da12dda730e65afe
Bruno <bruno_leonardosilva@hotmail.com>
2021-11-03 15:50:02 -0400

    Implementação de rota callback

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,17 @@
 import express from "express";
+import "dotenv/config"
 const app = express();
+
+app.get("/github", (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+app.get("/signin/callback", (req, res) => {
+    const { code } = req.query
+
+    return res.json(code)
+})
 
 app.listen(3003, () => console.log("Server Online!"));

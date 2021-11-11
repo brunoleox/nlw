c03394f3023e09944596f192f804873347f632ee
Bruno <bruno_leonardosilva@hotmail.com>
2021-11-10 21:40:05 -0400

    Criação de mensagens em tela.

--- frontend/src/components/MessageList/index.tsx
+++ frontend/src/components/MessageList/index.tsx
@@ -1,12 +1,23 @@
 import styles from './styles.module.scss'
 import logoImg from '../../assets/logo.png'
 import { api } from '../../services/api'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+
+type Message = {
+  id: string
+  text: string
+  user: {
+    name: string
+    avatar_url: string
+  }
+}
 
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([])
+
   useEffect(() => {
-    api.get('messages/last3').then((response) => {
-      console.log(response.data)
+    api.get<Message[]>('messages/last3').then((response) => {
+      setMessages(response.data)
     })
   }, [])
 
@@ -15,48 +26,19 @@ export function MessageList() {
       <img src={logoImg} alt="DoWhile 2021" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/brunoleox.png" alt="Avatar" />
-            </div>
-            <span>Bruno Leonardo</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/brunoleox.png" alt="Avatar" />
-            </div>
-            <span>Bruno Leonardo</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/brunoleox.png" alt="Avatar" />
-            </div>
-            <span>Bruno Leonardo</span>
-          </div>
-        </li>
+        {messages.map((message) => {
+          return (
+            <li className={styles.message} key={message.id}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
